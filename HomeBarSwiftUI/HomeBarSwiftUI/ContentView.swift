6f4428a37f88042f92afd77b404e01011c0da0f9
ILIA <dubenskiiilia@gmail.com>
2022-12-09 00:15:01 +0100

    some marks

--- HomeBarSwiftUI/HomeBarSwiftUI/ContentView.swift
+++ HomeBarSwiftUI/HomeBarSwiftUI/ContentView.swift
@@ -10,58 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var email = ""
-    @State private var password = ""
-
-    let homeBarLogoImage = "HomeBarLogo"
-    let cocktailsLogoImage = "CocktailsLogo"
-
-    let logInButton = "Log In"
-    let registrationButton = "Registration"
-
-    let neonOrange = "neonOrange"
-    let neonBlue = "neonBlue"
-
     var body: some View {
-        ScrollView{
-            VStack() {
-                Image(homeBarLogoImage)
-                    .resizable()
-                    .frame(width: 210, height: 200)
-                    .aspectRatio(contentMode: .fit)
-                Text("User with \(email) not exist!")
-                    .foregroundColor(Color(.systemRed))
-                    .lineLimit(1)
-                    .frame(width: 300)
-                    .hidden()
-                TextField("Email", text: $email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .foregroundColor(.black)
-                    .frame(width: 300.0)
-                SecureField("Password", text: $password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .foregroundColor(.black)
-                    .frame(width: 300.0)
-                Spacer()
-                    .frame(height: 20)
-                Button(logInButton) {
-                    print("hi")
-                }
-                .frame(width: 250, height: 40.0)
-                .background(Color(neonOrange))
-                .foregroundColor(Color.black)
-                .cornerRadius(10)
-                Button(registrationButton) {
-                    print("hi")
-                }
-                .frame(width: 150.0, height: 40.0)
-                .foregroundColor(Color(neonBlue))
-                Spacer()
+        ZStack() {
+            WallpaperView()
             }
-            .frame(width: 412) // пока захардкодил надо сравять фрейм стаквью с фрейм вью
         }
-        .background(Color.black)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
