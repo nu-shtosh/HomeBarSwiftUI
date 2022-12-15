3c7efa06431d2c9b91f5cf7158466c3b74fb1c05
Alexandr-Mayyura <sonickill@yandex.ru>
2022-12-15 14:58:34 +0300

    control commit

--- HomeBarSwiftUI/HomeBarSwiftUI/Views/ProfileView.swift
+++ HomeBarSwiftUI/HomeBarSwiftUI/Views/ProfileView.swift
@@ -14,6 +14,7 @@ struct ProfileView: View {
             WallpaperView()
             Text("Hello")
                 .foregroundColor(.white)
+           
             
         }
         .toolbar {
