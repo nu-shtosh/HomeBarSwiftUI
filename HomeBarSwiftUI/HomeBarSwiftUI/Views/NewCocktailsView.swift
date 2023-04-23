36654a0d97ad08da1b0f5a2dba140bd939c9c15b
Alexandr-Mayyura <sonickill@yandex.ru>
2023-04-23 14:21:29 +0300

    optimization

--- HomeBarSwiftUI/HomeBarSwiftUI/Views/NewCocktailsView.swift
+++ HomeBarSwiftUI/HomeBarSwiftUI/Views/NewCocktailsView.swift
@@ -13,8 +13,7 @@ struct NewCocktailsView: View {
     @StateObject var profileViewModel: ProfileViewModel
     @State private var image = Data()
     @State private var flag = false
-    @State var coincidence = 0
-
+    
     // MARK: - State Properties
     @State var isHidden = false
 
@@ -29,7 +28,6 @@ struct NewCocktailsView: View {
             ScrollView(.vertical, showsIndicators: false) {
                 
                 // MARK: - All Cocktails
-//                if newCocktailViewModel.cocktail.idUser == profileViewModel.profile.id {
                     Section {
                         LazyVGrid(columns: layout, spacing: 5) {
                             ForEach(Array(newCocktailViewModel.allCocktails), id: \.name) { item in
@@ -43,27 +41,16 @@ struct NewCocktailsView: View {
                                             CocktailCellView(cocktail: item, flag: $flag)
                                         }
                                     }
-                                } else if coincidence == 0 {
-                                    LabelView(text: "No cocktail")
                                 }
                             }
                         } // End LazyVGrid
                     } // End Section
                     .foregroundColor(Color("neonBlue"))
-//                } else {
-//                    LabelView(text: "no added cocktails")
-//                }
             } // End ScrollView1
             .navigationTitle("My cocktails")
             .navigationBarTitleDisplayMode(.large)
             .toolbar(.visible, for: .tabBar)
         } // End ZStack
-        .onAppear{
-           
-            print(profileViewModel.profile.id)
-            print(newCocktailViewModel.cocktail.idUser)
-            
-        }
     } // End Body
 }
 
